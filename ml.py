ff32c203aca6d52d8d3f0aa0faf799a589030b13
Stephen Blum <blum.stephen@gmail.com>
2025-10-06 13:27:17 -0700

    WE DID IT!!!!!!!!!!!🎉🎉🎉

--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@
 ## FROM SCRATCH PART 2: BACKPROPEGATION
 import numpy as np
 
-EPOCHS=1
+EPOCHS=100
 LEARN_RATE=0.01
 
 ## Artificail Nerual Network
@@ -28,7 +28,7 @@ class Layer():
         activation=np.tanh,
         derivitive=lambda x: 1 - np.tanh(x) ** 2,
     ):
-        self.bias = np.random.rand(output)
+        self.bias = np.random.rand(1, output)
         self.weights = np.random.rand(
             input,
             output,
@@ -42,11 +42,12 @@ class Layer():
 
     def backward(self, gradient):
         self.gradient = gradient
+        self.optimize()
         return np.dot(gradient, self.weights.T) * self.derivitive(self.input) 
 
-    def optimze():
-        self.weights += LEARN_RATE * self.input.T @ self.gradient
-        self.bias += LEARN_RATE * np.sum(self.gradient, axis=0, keepdims=True)
+    def optimize(self):
+        self.weights -= LEARN_RATE * self.input.T @ self.gradient
+        self.bias -= LEARN_RATE * np.sum(self.gradient, axis=0, keepdims=True)
 
 ## Model
 a = Layer(input=2, output=4)
@@ -61,33 +62,29 @@ for i in range(EPOCHS):
     out = a.forward(x)
     out = b.forward(out)
     out = c.forward(out)
-    print(out) ## AI's "answer"
+    #print(out) ## AI's "answer"
 
     ## delta (error)
     delta = out - y
-    print(delta)
+    #print(delta)
 
     ## loss for monitoring / cost
     loss = np.mean(np.square(delta))
-    ## good loss = below 1.0 approaching 0.1
+    ## good loss = decreasing, approaching 0.1
     ## bad loss = above 1.0 incrase beyond 1
     print(loss)
 
     ## First gradient
     gradient = delta * c.derivitive(out)
-    print(gradient)
+    #print(gradient)
 
     ## Backpropegation
     gradient = c.backward(gradient)
-    print(gradient)
+    #print(gradient)
 
     gradient = b.backward(gradient)
-    print(gradient)
+    #print(gradient)
 
     gradient = a.backward(gradient)
-    print(gradient)
-
-    #b_back = b.backward(delta)
-    #a_back = a.backward(delta)
-
+    #print(gradient)
 
